36534527f2a70df8521d4d67900460284eb4e74f
Flodger1 <sergeydiachenkos95@gmail.com>
2023-10-23 20:17:55 +0300

    ref 2

--- sudoku.js
+++ sudoku.js
@@ -1,31 +1,97 @@
 /* eslint-disable */
-const fs = require('fs');
+
+const fs = require("fs");
 
 function read() {
-  const path = './puzzles.txt';
+  const path = "./puzzles.txt";
   // извлекаем содержимое puzzles.txt
-  const board = fs.readFileSync(path, 'utf8');
+  const board = fs.readFileSync(path, "utf8");
   // разбиваем содержимое на отдельные строки
-  const minBoard = board.split('\n').filter((line) => line !== '');
+  const minBoard = board.split("\n").filter((line) => line !== "");
   // номер судоку
-  let minBoardNum = Number(process.argv[2]) || 2;
+  let minBoardNum = Number(process.argv[2]) || 15;
   // ограничиваем номер судоку (длина массива)
   if (minBoardNum > minBoard.length) {
     minBoardNum = minBoard.length;
   }
 
   const sudoku = minBoard[minBoardNum - 1];
-  console.log(`судоку № ${minBoardNum}`);
-  return sudoku;
+  return sudoku; 
 }
-console.log(read());
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+// isWork
+function transformFromSptring(str) {
+    const arr = [...str];
+  
+    const arrSize = 9;
+    const newArr = [];
+  
+    for (let i = 0; i < arr.length; i += arrSize) {
+      const periodArray = arr
+        .slice(i, i + arrSize)
+        .map((item) => (item === "-" ? 0 : parseInt(item, 10)));
+      newArr.push(periodArray);
+    }
+    return newArr;
+  }
+
+// **********************************************************************************************
+
+function isValid(board, row, column, number) {
+  for (let i = 0; i < 9; i++) {
+    if (board[row][i] === number || board[i][column] === number) {
+      return false;
+    }
+  }
+
+    const startRow = Math.floor(row / 3) * 3;
+    const startColumn = Math.floor(column / 3) * 3;
+    for (let j = 0; j < 3; j++) {
+      for (let k = 0; k < 3; k++) {
+        if (board[j + startRow][k + startColumn] === number) {
+          return false;
+        }
+      }
+    }
+  return true;
 }
+
+function solve(board) {
+  let emptyPlace = findEmpty(board);
+
+  // basic conditions for leaving
+  if (!emptyPlace) {
+    return board;
+  }
+
+  let row = emptyPlace[0];
+  let column = emptyPlace[1];
+
+  for (let num = 1; num <= 9; num++) {
+    if (isValid(board, row, column, num)) {
+      board[row][column] = num;
+
+      if (solve(board)) {
+        return board.map(el => el.join('|'))
+      }
+      board[row][column] = 0;
+    }
+  }
+  return false;
+
+  function findEmpty(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === 0) {
+          return [i, j];
+        }
+      }
+    }
+    return null;
+  }
+}
+
+// **********************************************************************************************
 
 function isSolved(board) {
   /**
@@ -42,103 +108,11 @@ function prettyBoard() {
    */
 }
 
-const fromSolve = [
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
-];
+const stringSudoku = read();
 
-[
-    [
-        [5, 3, 4], [6, 7, 8], [9, 1, 2]
-    ],
-    [
-        [6, 7, 2], [1, 9, 5], [3, 4, 8]
-    ],
-    [
-        [1, 9, 8], [3, 4, 2], [5, 6, 7]
-    ],
-    [
-        [8, 5, 9], [7, 6, 1], [4, 2, 3]
-    ],
-    [
-        [4, 2, 6], [8, 5, 3], [7, 9, 1]
-    ],
-    [
-        [7, 1, 3], [9, 2, 4], [8, 5, 6]
-    ],
-    [
-        [9, 6, 1], [5, 3, 7], [2, 8, 4]
-    ],
-    [
-        [2, 8, 7], [4, 1, 9], [6, 3, 5]
-    ],
-    [
-        [3, 4, 5], [2, 8, 6], [1, 7, 9]
-    ]
-];
+const board = transformFromSptring(stringSudoku);
 
+const solvedBoard = solve(board);
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-// isWork
-function transformFromSptring(str) {
-  const arr = [...str];
-
-  const arrSize = 9;
-  const newArr = [];
-
-  for(let i=0; i<arr.length; i+= arrSize) {
-    const periodArray = arr.slice(i,i+arrSize).map(item => item === '-' ? 0 : parseInt(item, 10));
-    newArr.push(periodArray)
-  }
-  return newArr
-}
-
-console.log(transformTo9x9('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'))
-
-
-function isValid(board, row, column, number) {
-  for (let i=0; i<row.length; i++) {
-    if (board[row][i] === number) {
-      return false 
-    }
-
-  for (let i =0; i>row.length; i++) {
-    if (board[column][i] === number) {
-      return false
-    }
-  }
-  }
-}
-
-
-
-
-
-
-// isWork
-// function transformTo3x3(arr) {
-
-// const arrSize = 3;
-
-// const newArr = []
-
-// for (let i=0; i<arr.length; i += arrSize){
-//   newArr.push(arr.slice(i, i + arrSize))
-// }
-// return newArr
-// }
-
-// console.log(transformTo3x3(arr));
-
-
-
+console.table(solvedBoard);
 
