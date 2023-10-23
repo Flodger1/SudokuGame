e19f37c075b6aa1e2f3af1bdd3cc5c4a65141167
AnzhelaKapranova <a.i.kapranova@gmail.com>
2023-10-23 15:58:26 +0300

    isSolved vertikal

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,12 @@
-/* eslint-disable */
-const fs = require('fs');
+eslint - disable;
+const fs = require("fs");
 
 function read() {
-  const path = './puzzles.txt';
+  const path = "./puzzles.txt";
   // извлекаем содержимое puzzles.txt
-  const board = fs.readFileSync(path, 'utf8');
+  const board = fs.readFileSync(path, "utf8");
   // разбиваем содержимое на отдельные строки
-  const minBoard = board.split('\n').filter((line) => line !== '');
+  const minBoard = board.split("\n").filter((line) => line !== "");
   // номер судоку
   let minBoardNum = Number(process.argv[2]) || 2;
   // ограничиваем номер судоку (длина массива)
@@ -34,30 +34,35 @@ function isSolved(board) {
    */
 }
 
-function gorizontCheck (arr) {
-  
-for( let i = 0; i < arr.length; i++){
-  const flattened = [].concat(...arr[i]);
-  const сheckNumbers = Array.from({ length: 9 }, (_, i) => i + 1)
-                          .every(num => flattened.includes(num));
-  if(!сheckNumbers) {
-    return false;
+function gorizontCheck(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const flattened = [].concat(...arr[i]);
+    const сheckNumbers = Array.from({ length: 9 }, (_, i) => i + 1).every(
+      (num) => flattened.includes(num)
+    );
+    if (!сheckNumbers) {
+      return false;
+    }
   }
-}  
-return true;
+  return true;
 }
 
 function vertikalCheck(arr) {
   const result = [];
 
-  for (let i =0; i<arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     const column = [];
-    for (let j =0; j<arr.length; j++) {
+    for (let j = 0; j < arr.length; j++) {
       column.push(arr[j][i]);
     }
-  result.push(column)
-  }
-  return result
+    result.push(column);
+    const сheckNumbers = Array.from({ length: 9 }, (_, i) => i + 1).every(
+      (num) => flattened.includes(num));
+    if (!сheckNumbers) {
+      return false;
+    }
+  }return true;
+  
 }
 console.log(vertikalCheck(arr));
 
@@ -78,42 +83,58 @@ const fromSolve = [
   [7, 1, 3, 9, 2, 4, 8, 5, 6],
   [9, 6, 1, 5, 3, 7, 2, 8, 4],
   [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
 ];
 
 [
-    [
-        [5, 3, 4], [6, 7, 8], [9, 1, 2]
-    ],
-    [
-        [6, 7, 2], [1, 9, 5], [3, 4, 8]
-    ],
-    [
-        [1, 9, 8], [3, 4, 2], [5, 6, 7]
-    ],
-    [
-        [8, 5, 9], [7, 6, 1], [4, 2, 3]
-    ],
-    [
-        [4, 2, 6], [8, 5, 3], [7, 9, 1]
-    ],
-    [
-        [7, 1, 3], [9, 2, 4], [8, 5, 6]
-    ],
-    [
-        [9, 6, 1], [5, 3, 7], [2, 8, 4]
-    ],
-    [
-        [2, 8, 7], [4, 1, 9], [6, 3, 5]
-    ],
-    [
-        [3, 4, 5], [2, 8, 6], [1, 7, 9]
-    ]
+  [
+    [5, 3, 4],
+    [6, 7, 8],
+    [9, 1, 2],
+  ],
+  [
+    [6, 7, 2],
+    [1, 9, 5],
+    [3, 4, 8],
+  ],
+  [
+    [1, 9, 8],
+    [3, 4, 2],
+    [5, 6, 7],
+  ],
+  [
+    [8, 5, 9],
+    [7, 6, 1],
+    [4, 2, 3],
+  ],
+  [
+    [4, 2, 6],
+    [8, 5, 3],
+    [7, 9, 1],
+  ],
+  [
+    [7, 1, 3],
+    [9, 2, 4],
+    [8, 5, 6],
+  ],
+  [
+    [9, 6, 1],
+    [5, 3, 7],
+    [2, 8, 4],
+  ],
+  [
+    [2, 8, 7],
+    [4, 1, 9],
+    [6, 3, 5],
+  ],
+  [
+    [3, 4, 5],
+    [2, 8, 6],
+    [1, 7, 9],
+  ],
 ];
 
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // isWork
 function transformFromSptring(str) {
@@ -122,34 +143,34 @@ function transformFromSptring(str) {
   const arrSize = 9;
   const newArr = [];
 
-  for(let i=0; i<arr.length; i+= arrSize) {
-    const periodArray = arr.slice(i,i+arrSize).map(item => item === '-' ? 0 : parseInt(item, 10));
-    newArr.push(periodArray)
+  for (let i = 0; i < arr.length; i += arrSize) {
+    const periodArray = arr
+      .slice(i, i + arrSize)
+      .map((item) => (item === "-" ? 0 : parseInt(item, 10)));
+    newArr.push(periodArray);
   }
-  return newArr
+  return newArr;
 }
 
-console.log(transformTo9x9('1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--'))
-
+console.log(
+  transformTo9x9(
+    "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+  )
+);
 
 function isValid(board, row, column, number) {
-  for (let i=0; i<row.length; i++) {
+  for (let i = 0; i < row.length; i++) {
     if (board[row][i] === number) {
-      return false 
+      return false;
     }
 
-  for (let i =0; i>row.length; i++) {
-    if (board[column][i] === number) {
-      return false
+    for (let i = 0; i > row.length; i++) {
+      if (board[column][i] === number) {
+        return false;
+      }
     }
-  }
   }
 }
-
-
-
-
-
 
 // isWork
 // function transformTo3x3(arr) {
@@ -165,7 +186,3 @@ function isValid(board, row, column, number) {
 // }
 
 // console.log(transformTo3x3(arr));
-
-
-
-
